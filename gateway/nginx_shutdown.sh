e767b4d4b811008ef9828f04f1c00e297ea4ee4f
Samuel Helén <78298409+samuelhelen@users.noreply.github.com>
2024-11-04 19:47:09 +0200

    Shut down nginx_gateway when upstream offline

---
+++ gateway/nginx_shutdown.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+# Used for stoping the nginx server if all service1 replicas are unavailable
+# curl -o /dev/null -s -w "%{http_code}\n" http://service1:8199/api
+
+while true; do
+	s1=`curl -o /dev/null -s -w "%{http_code}" http://condevade-service1-1:8199/api`
+	s2=`curl -o /dev/null -s -w "%{http_code}" http://condevade-service1-2:8199/api`
+	s3=`curl -o /dev/null -s -w "%{http_code}" http://condevade-service1-3:8199/api`
+
+	if [ "$s1" != "200" ] && [ "$s2" != "200" ] && [ "$s3" != "200" ]; then
+		nginx -s quit
+		systemctl halt --force
+	fi
+
+	sleep 5
+done
+
+
